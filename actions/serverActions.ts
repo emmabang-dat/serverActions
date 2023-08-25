ad13db23afaa93a8bad118d590c05798aa5e36d3
Emma Bang <83577644+emmabang-dat@users.noreply.github.com>
2023-08-25 11:54:10 +0200

    Server actions

---
+++ actions/serverActions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { Product } from "@/typings";
+import { revalidateTag } from "next/cache";
+
+export const addProductToDatabase = async (e: FormData) => {
+
+    const product = e.get("product")?.toString();
+    const price = e.get("price")?.toString();
+
+    if (!product || !price) return;
+
+    const newProduct: Product = {
+      product,
+      price,
+    };
+    await fetch('https://64e857d599cf45b15fdf68a3.mockapi.io/products', {
+      method: 'POST',
+      body: JSON.stringify(newProduct),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    revalidateTag("products");
+  };
